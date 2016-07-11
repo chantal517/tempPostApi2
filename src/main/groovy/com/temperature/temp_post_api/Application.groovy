5f82e53519b646db3131b157e76b110ce6fad2ea
chantal517 <Chantal@Chantals-MacBook-Pro.local>
2016-07-10 20:44:29 -0500

    Adding application class

---
+++ src/main/groovy/com/temperature/temp_post_api/Application.groovy
@@ -0,0 +1,10 @@
+package com.temperature.temp_post_api
+
+/**
+ * Created by Chantal on 7/10/16.
+ */
+class Application {
+    static def void main(String[] args) {
+        println("Hello world")
+    }
+}
