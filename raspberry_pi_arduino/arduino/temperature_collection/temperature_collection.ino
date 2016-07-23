0fa46070377815ec6bad7d25c6bae86a0aed7b85
n02870941 <jabaridash>
2016-07-22 23:20:39 -0500

    Added rpi folder

---
+++ raspberry_pi_arduino/arduino/temperature_collection/temperature_collection.ino
@@ -0,0 +1,24 @@
+const int TEMPERATURE_COLLECTION_DELAY = 1000 * 60;
+const int TEMPERATURE_POST_DELAY = TEMPERATURE_COLLECTION_DELAY * 10;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+public double getTemperature() {
+  return 1.0;
+}
+
+public void logTemperature(dpuble temperature) {
+  ;
+}
+
+public boolean sendTemperatureToServer() {
+  // TODO - Implemenet logic for sending temperature data to sever
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
